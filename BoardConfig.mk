dd277fb74b5b325fa07945250d95338c06fc3b61
danascape <saalimquadri1@gmail.com>
2021-02-09 10:54:33 +0100

    X01AD: Set tap to wakeup node
    Change-Id: I1e1618480728a2888137c84335632499cb164819

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,6 +187,9 @@ TARGET_BOARD_PLATFORM := msm8953
 TARGET_USES_INTERACTION_BOOST := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/tpd_gesture"
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
