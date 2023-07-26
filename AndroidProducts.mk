fdb3aa1f9aafa9ede72e1bbdd90d3d6b23e5810d
Daniel <doodle280204@gmail.com>
2023-07-26 23:27:53 +0000

    X01AD: Adapt Lineage Tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_X01AD.mk
+    $(LOCAL_DIR)/lineage_X01AD.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_X01AD-user \
-    derp_X01AD-userdebug \
-    derp_X01AD-eng
+    lineage_X01AD-user \
+    lineage_X01AD-userdebug \
+    lineage_X01AD-eng
--- lineage_X01AD.mk
+++ lineage_X01AD.mk
@@ -21,18 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from X01AD device
 $(call inherit-product, device/asus/X01AD/device.mk)
 
-# Inherit some common DerpfestOS stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Set shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Derpfest Flags
-TARGET_BOOT_ANIMATION_RES := 720
-USE_LEGACY_BOOTANIMATION := true
-
 # Device identifiers.
-PRODUCT_NAME := derp_X01AD
+PRODUCT_NAME := lineage_X01AD
 PRODUCT_DEVICE := X01AD
 PRODUCT_MANUFACTURER := asus
 PRODUCT_BRAND := asus
